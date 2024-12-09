d896bd53aec170a562f61b5b2eaf8c2dd72fef12
Koch Viktor <viktor.koch@oxit.hu>
2024-12-09 16:30:16 +0100

    Add schedule router to main router

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from .camera import router as camera_router
+from .schedule import router as schedule_router
 
 router = APIRouter(prefix="/v1")
+router.include_router(schedule_router)
 router.include_router(camera_router)
