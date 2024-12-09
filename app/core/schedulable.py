8c57288fddf12b96b10d9890e6f8fd3ab5e49365
Koch Viktor <viktor.koch@oxit.hu>
2024-12-09 14:01:54 +0100

    Modify Schedulable

--- app/core/schedulable.py
+++ app/core/schedulable.py
@@ -1,12 +1,16 @@
 import abc
 
 
-@abc.abstractmethod
-class Schedulable:
+class Schedulable(abc.ABC):
     @abc.abstractmethod
     def start(self, *args, **kwargs):
         pass
 
     @abc.abstractmethod
     def stop(self, *args, **kwargs):
+        pass
+
+    @property
+    @abc.abstractmethod
+    def is_running(self) -> bool:
         pass
