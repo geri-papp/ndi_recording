3255ba8d74cd1be3361009844f80735ed9837899
Koch Viktor <viktor.koch@oxit.hu>
2024-12-10 13:49:59 +0100

    Remove duration for now

--- app/schemas/schedule.py
+++ app/schemas/schedule.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime#, timedelta
 from typing import Annotated
 
 from pydantic import BaseModel, Field, model_validator
@@ -59,58 +59,45 @@ class SetSchedule(BaseModel):
         ),
     ]
     end_time: Annotated[
-        datetime | None,
+        datetime,# | None,
         Field(
             description="End time of the schedule",
             examples=["2021-08-01T13:30:00"],
         ),
     ]
-    duration: Annotated[
-        timedelta | None,
-        Field(
-            description="Duration of the schedule",
-            examples=["1:00:00"],
-        ),
-    ]
-
+    # duration: Annotated[
+    #     timedelta | None,
+    #     Field(
+    #         description="Duration of the schedule",
+    #         examples=["1:00:00"],
+    #     ),
+    # ]
+    #
     @model_validator(mode="before")
     @classmethod
     def validate_schedule(cls, data: dict) -> dict:
         start_time_str: str = data["start_time"]
-        end_time_str: str | None = data.get("end_time")
-        duration_str: str | None = data.get("duration")
+        end_time_str: str = data["end_time"]
+        # duration_str: str | None = data.get("duration")
         start_time: datetime = datetime.fromisoformat(start_time_str)
-        end_time: datetime | None = datetime.fromisoformat(end_time_str) if end_time_str else None
-        duration_datetime: datetime | None = datetime.fromisoformat(duration_str) if duration_str else None
-        duration: timedelta | None = (
-            duration_datetime - datetime.fromisoformat("1900-01-01T00:00:00") if duration_datetime else None
-        )
+        end_time: datetime = datetime.fromisoformat(end_time_str)# if end_time_str else None
+        # duration_datetime: datetime | None = datetime.fromisoformat(duration_str) if duration_str else None
+        # duration: timedelta | None = (
+        #     duration_datetime - datetime.fromisoformat("1900-01-01T00:00:00") if duration_datetime else None
+        # )
 
-        if end_time is not None and duration is not None:
-            raise ValueError("Either end_time or duration should be provided, not both.")
-        if end_time is None and duration is None:
-            raise ValueError("Either end_time or duration should be provided.")
+        # if end_time is not None and duration is not None:
+        #     raise ValueError("Either end_time or duration should be provided, not both.")
+        # if end_time is None and duration is None:
+        #     raise ValueError("Either end_time or duration should be provided.")
 
-        if end_time is not None:
-            duration = end_time - start_time
-        if duration is not None:
-            end_time = start_time + duration
+        # if end_time is not None:
+        #     duration = end_time - start_time
+        # if duration is not None:
+        #     end_time = start_time + duration
 
-        assert end_time is not None
+        # assert end_time is not None
         if end_time < start_time:
             raise ValueError("end_time should be greater than start_time.")
 
-        return {"start_time": start_time, "end_time": end_time, "duration": duration}
-
-    def to_schedule(self) -> Schedule:
-        if self.end_time is not None and self.duration is not None:
-            raise ValueError("Either end_time or duration should be provided, not both.")
-
-        if self.end_time is not None:
-            self.duration = self.end_time - self.start_time
-
-        if self.duration is not None:
-            self.end_time = self.start_time + self.duration
-
-        assert self.end_time is not None
-        return Schedule(start_time=self.start_time, end_time=self.end_time)
+        return {"start_time": start_time, "end_time": end_time}
