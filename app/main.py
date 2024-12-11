d2b8539afbd93bde3236776d3268b91c6f98c4eb
Koch Viktor <viktor.koch@oxit.hu>
2024-12-11 12:06:27 +0100

    Add custom unique id function

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from .api import router as api_router
+from .core.utils.custom_unique_id import custom_generate_unique_id
 
-app = FastAPI()
+app = FastAPI(generate_unique_id_function=custom_generate_unique_id)
 app.include_router(api_router)
