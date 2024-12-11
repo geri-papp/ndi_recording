584636cd011178c4e5fec5e75ece3a28abb6f262
Koch Viktor <viktor.koch@oxit.hu>
2024-12-11 16:27:46 +0100

    Add fields to FastAPI, change title

--- app/main.py
+++ app/main.py
@@ -3,5 +3,13 @@ from fastapi import FastAPI
 from .api import router as api_router
 from .core.utils.custom_unique_id import custom_generate_unique_id
 
-app = FastAPI(generate_unique_id_function=custom_generate_unique_id)
+app = FastAPI(
+    generate_unique_id_function=custom_generate_unique_id,
+    title="OXCAM",
+    version="0.1.0",
+    contact={
+        "name": "OX-IT",
+        "email": "viktor.koch@oxit.hu"
+    },
+)
 app.include_router(api_router)
